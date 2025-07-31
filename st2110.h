5624210b2b6a58c09a010e478648ac9d9bfd955a
ZzLee <zzlee.tw@gmail.com>
2025-07-31 09:31:54 +0800

    Some function spec modifications

--- st2110.h
+++ st2110.h
@@ -11,73 +11,85 @@ extern "C" {
 #endif
 
 typedef enum {
-  WT_OK = 0,
-  WT_ERR_INIT = -1,
-  WT_ERR_PARAM = -2,
-  WT_ERR_NOMEM = -3,
-  WT_ERR_INTERNAL = -4,
-  WT_ERR_TIMEOUT = -5
+	WT_OK = 0,
+	WT_ERR_INIT = -1,
+	WT_ERR_PARAM = -2,
+	WT_ERR_NOMEM = -3,
+	WT_ERR_INTERNAL = -4,
+	WT_ERR_TIMEOUT = -5
 } wt_status_t;
 
 typedef enum {
-  WT_PROTOCOL_ST20,
-  WT_PROTOCOL_ST22,
-  WT_PROTOCOL_ST30,
-  WT_PROTOCOL_ST31
+	WT_PROTOCOL_ST20,
+	WT_PROTOCOL_ST22,
+	WT_PROTOCOL_ST30,
+	WT_PROTOCOL_ST31
 } wt_protocol_t;
 
 typedef enum {
-  WT_CODEC_RAW,
-  WT_CODEC_H264,
-  WT_CODEC_H265,
-  WT_CODEC_PCM,
-  WT_CODEC_LPCM
+	WT_CODEC_RAW,
+	WT_CODEC_H264,
+	WT_CODEC_H265,
+	WT_CODEC_PCM,
+	WT_CODEC_LPCM,
+	// WT_CODEC_TIMECODE,
+	// WT_CODEC_SUBTITLE,
+
+	WT_CODEC_UNDEFINED = 0xFFFFFFFF
 } wt_codec_t;
 
-typedef void* wt_handle_t;
+typedef enum {
+	WT_CONNECTION_STATUS_CONNECTED,
+	WT_CONNECTION_STATUS_DISCONNECTED,
+} wt_connection_status_t;
+
+typedef struct wt_tx wt_tx_t; // opaque
+typedef struct wt_tx wt_rx_t; // opaque
 
 typedef struct {
-  void* data;
-  size_t size;
-  uint64_t timestamp_ns;
-  int width;   // only for video
-  int height;  // only for video
-  int channels; // only for audio
-  int sample_rate; // only for audio
-} wt_frame_t;
+	int codec,
+
+	void* data;
+	size_t size;
+	uint64_t timestamp_ns;
+} wt_packet_t;
 
 // Rx callback type
-typedef void (*wt_rx_callback_t)(wt_handle_t handle, const wt_frame_t* frame, void* user_data);
+typedef void (*wt_rx_connection_status_callback_t)(wt_connection_status_t status, void* user_data);
+typedef void (*wt_rx_packet_callback_t)(const wt_packet_t* packet, void* user_data);
 
 //
 // TX
 //
-wt_status_t wt_tx_create(wt_protocol_t proto, wt_codec_t codec,
-                         const char* local_ip, const char* dest_ip, int port,
-                         int width, int height, int fps,
-                         wt_handle_t* handle);
+wt_status_t wt_tx_create(wt_protocol_t proto, const char* local_ip, const char* dest_ip, int port, wt_tx_t** ppThis);
+wt_status_t wt_tx_destroy(wt_tx_t* pThis);
 
-wt_status_t wt_tx_start(wt_handle_t handle);
-wt_status_t wt_tx_push_frame(wt_handle_t handle, const uint8_t* data, size_t size, uint64_t timestamp_ns);
-wt_status_t wt_tx_push_packet(wt_handle_t handle, const uint8_t* data, size_t size, uint64_t timestamp_ns);
-wt_status_t wt_tx_stop(wt_handle_t handle);
-wt_status_t wt_tx_destroy(wt_handle_t handle);
+wt_status_t wt_tx_set_video_format(wt_tx_t* pThis, wt_codec_t codec, int width, int height, int fps_num, int fps_den);
+wt_status_t wt_tx_set_audio_format(wt_tx_t* pThis, wt_codec_t codec, int bitcount, int channels, int frequency);
+// wt_status_t wt_tx_set_timecode_format(wt_tx_t* pThis, wt_codec_t codec, ...);
+// wt_status_t wt_tx_set_subtitle_format(wt_tx_t* pThis, wt_codec_t codec, ...);
+
+wt_status_t wt_tx_start(wt_tx_t* pThis);
+wt_status_t wt_tx_stop(wt_tx_t* pThis);
+
+wt_status_t wt_tx_send_packet(wt_tx_t* pThis, wt_packet_t* pPacket);
 
 //
 // RX
 //
-wt_status_t wt_rx_create(wt_protocol_t proto, wt_codec_t codec,
-                         const char* local_ip, const char* source_ip, int port,
-                         int width, int height, int fps,
-                         wt_handle_t* handle);
+wt_status_t wt_rx_create(wt_protocol_t proto, const char* local_ip, const char* source_ip, int port, wt_rx_t** ppThis);
+wt_status_t wt_rx_destroy(wt_rx_t* pThis);
 
-wt_status_t wt_register_rx_buffers(wt_handle_t handle, void** buffers, int buffer_count, size_t buffer_size);
-wt_status_t wt_register_rx_frame_callback(wt_handle_t handle, wt_rx_callback_t cb, void* user_data);
-wt_status_t wt_register_rx_packet_callback(wt_handle_t handle, wt_rx_callback_t cb, void* user_data);
+wt_status_t wt_rx_set_connection_status_callback(wt_rx_t* pThis, wt_rx_connection_status_callback_t cb, void* user_data);
+wt_status_t wt_rx_set_packet_callback(wt_rx_t* pThis, wt_rx_packet_callback_t cb, void* user_data);
 
-wt_status_t wt_rx_start(wt_handle_t handle);
-wt_status_t wt_rx_stop(wt_handle_t handle);
-wt_status_t wt_rx_destroy(wt_handle_t handle);
+wt_status_t wt_rx_start(wt_rx_t* pThis);
+wt_status_t wt_rx_stop(wt_rx_t* pThis);
+
+wt_status_t wt_rx_get_video_format(wt_rt* pThis, wt_codec_t* pCodec, int* pWidth, int* pHeight, int* pFpsNum, int* pFpsDen);
+wt_status_t wt_rx_get_audio_format(wt_rt* pThis, wt_codec_t* pCodec, int* pBitCount, int* pChannels, int* pFrequenct);
+// wt_status_t wt_rx_get_timecode_format(wt_rt* pThis, wt_codec_t* pCodec, ...);
+// wt_status_t wt_rx_get_subtitle_format(wt_rt* pThis, wt_codec_t* pCodec, ...);
 
 #ifdef __cplusplus
 }
